0c7035a92f95a00915c412e9c0a4bf3ea5c7cf70
ailequal <37016865+ailequal@users.noreply.github.com>
2020-02-23 10:33:51 +0100

    Added snack-03.php

--- snack-03.php
+++ snack-03.php
@@ -4,7 +4,20 @@
  Prendere il paragrafo e suddividerlo in tanti paragrafi <p>. Ogni punto un nuovo paragrafo. -->
 
 <?php
-	echo 'hello world';
+	$lorem = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut fugiat ullam neque maxime quod ratione ipsam labore corrupti quidem vel. Modi cum sequi id mollitia iusto quo illo ad facilis.';
+	// dived the $lorem into little paragraphs with the dot
+	$array_lorem = (explode('. ', $lorem));
+	for ($i = 0; $i < count($array_lorem); $i++) {
+		if ($i === 0) {
+			$string0 = $array_lorem[$i];
+			$string0 .= '.'; // add missing dot
+		} elseif ($i === 1) {
+			$string1 = $array_lorem[$i];
+			$string1 .= '.'; // add missing dot
+		} elseif ($i === 2) {
+			$string2 = $array_lorem[$i];
+		}
+	}
 ?>
 
 <!DOCTYPE html>
@@ -23,6 +36,8 @@
 </head>
 <body>
 	<h1>snack-03 - php-get-params</h1>
-	<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut fugiat ullam neque maxime quod ratione ipsam labore corrupti quidem vel. Modi cum sequi id mollitia iusto quo illo ad facilis.</p>
+	<p><?php echo $string0 ?></p>
+	<p><?php echo $string1 ?></p>
+	<p><?php echo $string2 ?></p>
 </body>
 </html>
